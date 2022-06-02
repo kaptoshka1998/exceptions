004e9fe25cca535e241677c25603f4d0b661a77b
Darya <allexxika@yandex.ru>
2022-06-02 13:21:38 +0300

    Initial commit

---
+++ src/ru/skypro/Main.java
@@ -0,0 +1,12 @@
+package ru.skypro;
+
+public class Main {
+
+    public static void object(String login, String password,String confirmPassword ){
+    }
+
+
+    public static void main(String[] args) {
+        object("java_skypro.go", "java1_skypro.go", "java2_skypro.go");
+    }
+}
